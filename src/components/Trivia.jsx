26f4eb43a33530e39005d1dd40d338db0a76782c
uparlayan <ugurparlayan@gmail.com>
2023-12-24 15:29:53 +0300

    İlk commit, devamı gelecek

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">soru soru soru soru soru soru soru</div>
+      <div className="answers">
+        <div className="answer">cevap cevap</div>
+        <div className="answer">cevap cevap</div>
+        <div className="answer">cevap cevap</div>
+        <div className="answer">cevap cevap</div>
+      </div>
+    </div>
+  );
+}
